1d9f42362681ef8658cb1e0c6fab7a3d6013cc44
Charlie Croom <charlie.croom@cityblock.com>
2023-05-17 14:27:46 -0400

    change

--- packages/header/src/Header.tsx
+++ packages/header/src/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export function Header() {
     return (
         <header>
-            Lerna is the original monorepo tool! Nice!
+            Lerna is the original monorepo tool! Nice2!
         </header>
     );
 }
